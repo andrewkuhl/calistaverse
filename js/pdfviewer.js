9f92732fa1423fd717169d1f8769d291672cab34
andrewkuhl <andrewkuhlz@gmail.com>
2022-03-18 13:12:34 -0700

    issues

--- js/pdfviewer.js
+++ js/pdfviewer.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $('#paper').click(function(){
-          $('#Mymodal').modal('show')
+const pdfs = document.querySelectorAll(".pdfs object");
+const images = document.querySelectorAll(".gridwrapper object");
+const modal = document.querySelector(".modal");
+const modalpdf = document.querySelector(".modalpdf");
+const close = document.querySelector(".close");
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modal.classList.add("appear");
+        
+        close.addEventListener("click", () => {
+            modal.classList.remove("appear");
+        });
     });
 });

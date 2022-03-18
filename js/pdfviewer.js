d80d2701675b9476683e7f4d9448943c313a9f67
andrewkuhl <andrewkuhlz@gmail.com>
2022-03-18 15:51:14 -0700

    modalpdf data changes with each picture click but pdf doesnt rerender. solution for now is refreshing the page but it feels bad,

--- js/pdfviewer.js
+++ js/pdfviewer.js
@@ -4,12 +4,31 @@ const modal = document.querySelector(".modal");
 const modalpdf = document.querySelector(".modalpdf");
 const close = document.querySelector(".close");
 
+$('#Mymodal').on('hidden.bs.modal', function () {
+    location.reload();
+})
+
 images.forEach((image) => {
+    
     image.addEventListener("click", () => {
-        modal.classList.add("appear");
+
+        let imgname = image.getAttribute("name");
         
-        close.addEventListener("click", () => {
-            modal.classList.remove("appear");
+        console.log(imgname);
+        
+        pdfs.forEach((pdf) => {
+            if(pdf.name == imgname){
+                console.log(modalpdf.data);
+                console.log(pdf.data);
+                $(".modalpdf").attr({
+                    data: pdf.data
+                });
+                console.log("found" + pdf.name);
+                
+            }
+            $('#Mymodal').modal('show');
         });
+
     });
 });
+
